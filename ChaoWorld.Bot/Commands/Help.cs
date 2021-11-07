9b9728a2d92ed42b5b7baeb6b472747d45b232e7
Noko <nephanim@gmail.com>
2021-11-07 07:53:39 -0600

    Added credit to footer of help command

--- ChaoWorld.Bot/Commands/Help.cs
+++ ChaoWorld.Bot/Commands/Help.cs
@@ -15,7 +15,7 @@ namespace ChaoWorld.Bot
                 .Field(new("How do I get one?", "Try using the following commands:\n**•** `!garden new` - Create a garden (if you haven't already)\n**•** `!garden list` - See a list of chao in your garden\n**•** `!chao Unnamed name {name}` - Pick a name for your chao (you can always change it later)\n**•** `!chao {name}` - See details about your new chao\n**•** `!garden default {chao name}` - Set this chao as your default for races and items"))
                 .Field(new("Can I get more chao?", "While the starter chao comes with your garden, you'll have to purchase eggs from the Black Market with rings to get additional chao. Use `!collect` to gather rings once per day. You will also earn rings by participating in races and tournaments."))
                 .Field(new("How do I use the bot?", "For a full list of commands, see [the command list](https://bytebarcafe.com/chao/commands.php). If you have any questions, just ask!"))
-                .Footer(new($"Sonic the Hedgehog and Chao are property of Sega and Sonic Team."))
+                .Footer(new($"Created by Noko for Byte Bar & Cafe. Sonic the Hedgehog and Chao are property of Sega and Sonic Team."))
                 .Color(DiscordUtils.Blue)
                 .Build());
         }
