87a0fe53a2bf3559ee2ecd5f7a882500f2472918
Noko <nephanim@gmail.com>
2021-12-16 02:04:50 -0600

    Reduced daily instance limit
    This is an important protection against scripts/bots flying ahead of legit players. It's definitely possible to reach 100 races in a day, but even playing obsessively it's hard to do. A script can easily make it to 200+ which is a problem without a lower limit.

--- ChaoWorld.Core/Database/Repository/ModelRepository.Garden.cs
+++ ChaoWorld.Core/Database/Repository/ModelRepository.Garden.cs
@@ -89,7 +89,7 @@ namespace ChaoWorld.Core
         {
             var query = new Query("gardens").AsUpdate(new
             {
-                instancelimit = 200
+                instancelimit = 100
             });
             await _db.Query<Garden>(query);
             _logger.Information("Reset instance limits for all gardens");
